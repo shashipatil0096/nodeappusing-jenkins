d363b316523ad666d35924d206bfed2e406fc68a
shashipatil0096 <shashipatil0096@mail.com>
2025-12-02 07:25:02 +0000

    my commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("CI/CD welcome to port no 3000");
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
